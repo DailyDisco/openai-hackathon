99bad92673acab7a14a1131177b919f0639e7c66
Diego (D) <actual.mensch0r@icloud.com>
2022-10-16 10:37:44 -0500

    add flask and docker api 3 in 1 folder

---
+++ app/whisper-api/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, abort, request
+from tempfile import NamedTemporaryFile
+import whisper
+import torch
+import gpt3
+import openai
+import os
+import codex
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Check if NVIDIA GPU is available
+torch.cuda.is_available()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load the Whisper model:
+model = whisper.load_model("base", device=DEVICE)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Whisper Hello World!"
+
+
+@app.route('/whisper', methods=['POST'])
+def handler():
+    if not request.files:
+        # If the user didn't submit any files, return a 400 (Bad Request) error.
+        abort(400)
+
+    # For each file, let's store the results in a list of dictionaries.
+    results = []
+
+    # Loop over every file that the user submitted.
+    for filename, handle in request.files.items():
+        # Create a temporary file.
+        # The location of the temporary file is available in `temp.name`.
+        temp = NamedTemporaryFile()
+        # Write the user's uploaded file to the temporary file.
+        # The file will get deleted when it drops out of scope.
+        handle.save(temp)
+        # Let's get the transcript of the temporary file.
+        result = model.transcribe(temp.name)
+        text = result['text']
+        # Let's get the summary of the soundfile
+        summary = gpt3.gpt3complete(text)
+        
+        secondSummary = codex.codexComplete(summary.strip())
+        # Now we can store the result object for this file.
+        results.append({
+            'filename': filename,
+            'transcript': text.strip(),
+            'summary': summary.strip(),
+            'secondSummary': secondSummary.strip(),
+        })
+
+    # This will be automatically converted to JSON.
+    return {'results': results}
--- app/whisper_api.py
+++ app/whisper_api.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException
-from whisper_mic import main #, generate_keywords
+from fastapi import FastAPI, HTTPException, File, UploadFile
+from whisper_mic import whisperResults, main #, generate_keywords
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 handler = Mangum(app) # this wraps the api app in a handler function
@@ -15,11 +17,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# @app.get('/whisper_snippet')
+# async def generate_whisper_api(result):
+#     # validate_input_length(result)
+#     snippet = main(result)
+#     return {"snippet": snippet}
+
+# @app.post("/files/")
+# async def create_file(file: bytes = File()):
+#     return {"file_size": len(file)}
+
+
+# @app.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile):
+#     return {"filename": file.filename}
+
 @app.get('/whisper_snippet')
-async def generate_whisper_api(result: str):
-    validate_input_length(result)
-    snippet = main(result)
-    return {"snippet": snippet, "keywords": []}
+async def generate_whisper_api(result):
+    # validate_input_length(result)
+    snippet = whisperResults(result)
+    return {"snippet": snippet}
 
 # validates the length of the prompt and throws an error if it's too long
 def validate_input_length(result: str):
